c02a189465c6538695cc58629d963e8101ac2757
willber10 <willberschickler@gamil.com>
2024-01-21 13:33:51 -0800

    imported fs and created get and post request

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 const express = require('express');
 
+const fs = require('fs');
+
+const path = require('path');
+
 const port = process.env.PORT || 3001;
 
 const app = express();
@@ -14,6 +18,18 @@ app.get('/notes', (req, res) => {
   res.sendFile(path.join(__dirname, '/public/notes.html'));
 });
 
+
+app.post('/api/notes', (req, res) => {
+  res.send('POST request to the homepage');
+}); 
+
+app.get('/api/notes', (req, res) => {
+ let notes = fs.readFileSync('./db/db.json', 'utf8');
+ notes = JSON.parse(notes);
+  res.json(notes);
+});
+
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '/public/index.html'));
 });
+
